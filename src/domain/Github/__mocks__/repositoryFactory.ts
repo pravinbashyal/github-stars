3dd884ce7cdea5f3bff34b4b282ff43b63ed3ff0
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-04-26 01:08:57 +0200

    add repository factory

---
+++ src/domain/Github/__mocks__/repositoryFactory.ts
@@ -0,0 +1,23 @@
+// factories/user.ts
+import { Factory } from 'fishery'
+import { Language } from '../Language'
+import { Repository } from '../Repository'
+// import postFactory from './post';
+
+export const repositoryFactory = Factory.define<Repository>(({ sequence }) => ({
+  id: `repo-${sequence}`,
+  name: `Popular Guy-${sequence}`,
+  full_name: `popular_guy-${sequence}`,
+  private: false,
+  owner: {
+    login: `popular_guy-${sequence}`,
+    id: `owner-${sequence}`,
+    avatar_url: '',
+  },
+  description: `Some famouse repository description - ${sequence}`,
+  stargazers_count: 10,
+  watchers_count: 12,
+  language: Language.Go,
+  forks: 2,
+  default_branch: 'main',
+}))
