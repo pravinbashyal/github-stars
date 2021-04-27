b3d4e6d966d6b13bec1f329931023b50c69f1f5c
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-04-27 18:11:31 +0200

    add wrapper for test

--- src/testSetup/wrapper.tsx
+++ src/testSetup/wrapper.tsx
@@ -1,4 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import React, { FC } from 'react'
+import { FavoritesDbProvider } from '../common-infra/useFavoritesDb'
+import { AlertProvider } from '../common-components/Alert/state/useAlert'
 
-export const Wrapper: FC<{}> = ({ children }) => <Router>{children}</Router>
+export const Wrapper: FC<{}> = ({ children }) => (
+  <AlertProvider>
+    <FavoritesDbProvider>
+      <Router>{children}</Router>
+    </FavoritesDbProvider>
+  </AlertProvider>
+)
