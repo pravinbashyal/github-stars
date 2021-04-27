db3660b03b8950d05643ca51389184fa4f7b5735
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-04-27 19:05:17 +0200

    fix default navbar

--- src/Layout/NavBar.tsx
+++ src/Layout/NavBar.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Menu } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link, useHistory, useLocation } from 'react-router-dom'
+import { findKey } from 'lodash-es'
 
-export const NavBar = () => (
-  <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-    <Menu.Item key="1">
-      <Link to="/popular">Popular</Link>
-    </Menu.Item>
-    <Menu.Item key="2">
-      <Link to="/favorites">Favorites</Link>
-    </Menu.Item>
-  </Menu>
-)
+enum NavMenu {
+  Popular = 'popular',
+  Favorites = 'favorites',
+}
+
+const navMenuUrl: Record<NavMenu, string> = {
+  [NavMenu.Popular]: '/popular',
+  [NavMenu.Favorites]: '/favorites',
+}
+
+export const NavBar = () => {
+  const [selectedTab, setSelectedTab] = React.useState(NavMenu.Popular)
+  const location = useLocation()
+  const history = useHistory()
+  useEffect(() => {
+    const navMenu = findKey(navMenuUrl, (value) => value === location.pathname)
+    if (!navMenu) {
+      return
+    }
+    setSelectedTab(navMenu as NavMenu)
+  }, [selectedTab, location])
+
+  const navigateTab = (navMenu: NavMenu) => {
+    history.push(navMenuUrl[navMenu])
+  }
+  return (
+    <Menu theme="dark" mode="horizontal" selectedKeys={[selectedTab]}>
+      <Menu.Item key={NavMenu.Popular} onClick={() => navigateTab(NavMenu.Popular)}>
+        Popular
+      </Menu.Item>
+      <Menu.Item key={NavMenu.Favorites}>
+        <Link to={`/${NavMenu.Favorites}`}>Favorites</Link>
+      </Menu.Item>
+    </Menu>
+  )
+}
